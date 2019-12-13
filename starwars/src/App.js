89eee84423819a8f5aa49dbfb6b98bff3a663ab7
Aaron Andersen <anderseana@tutanota.com>
2019-12-13 11:49:11 -0600

    installed reactstrap and axios

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -3,15 +3,18 @@ import CharList from './components/charlist';
 import Style from './components/styles';
 import './App.css';
 
-// Grabbing API here.
+// Setting the state.
 const App = () => {
-  const [peopers, setPeopers] = useState([]);
-
+  const [peepers, setPeepers] = useState([]);
+// Grabbing the API with axios.
   useEffect(() => {
     axios.get('https://swapi.co/api/people/?page=')
-    .then(response =>
-      )
-  })
+    .then(response => {
+      console.log(response);
+      setPeepers(response.data.results);
+    })
+    .catch(err => console.log({err}));
+  },[])
 
   return (
     <div className="App">
