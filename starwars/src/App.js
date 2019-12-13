11554afab1dc72cf691e8d683c1b3d163aa7b8bd
Aaron Andersen <anderseana@tutanota.com>
2019-12-13 11:39:11 -0600

    scrapped the whole thing and rewrote appjs

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,24 +1,22 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
+import CharList from './components/charlist';
+import Style from './components/styles';
 import './App.css';
 
 // Grabbing API here.
-const App = (Component) => {
-  constructor() {
-    super();
-    this.state = {
-      starWarsCharacters: [],
-      url: 'https://swapi.co/api/people/?page=', current: 1,
-      firstPage: 'https://swapi.co/api/people/',
-    };
-  }
-  // Going to mount the getCharacters API
-  componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people/')
-  }
+const App = () => {
+  const [peopers, setPeopers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.co/api/people/?page=')
+    .then(response =>
+      )
+  })
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {/* <CharList characters={this.state.starWarsCharacters}/> */}
     </div>
   );
 }
